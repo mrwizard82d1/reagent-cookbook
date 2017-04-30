5a6b222af00a17a5615185d772b3402e494ba4dd
Larry Jones <larryj@blinds.com>
2017-04-30 17:15:05 -0500

    Initial project structure

---
+++ basic-component/src/cljs/basic_component/core.cljs
@@ -0,0 +1,12 @@
+(ns basic-component.core
+    (:require [reagent.core :as reagent]))
+
+(defn home []
+  [:div
+   
+   ])
+
+(defn ^:export main []
+  (reagent/render [home]
+                  (.getElementById js/document "app")))
+
