0b9afa33ccf791af8ec03c2af9f7c5de6ab6798f
Larry Jones <larryj@blinds.com>
2017-04-30 18:10:23 -0500

    Finished basic components
    This code is an example of three *different* ways to create three
    *equivalent* React components.

--- basic-component/src/cljs/basic_component/core.cljs
+++ basic-component/src/cljs/basic_component/core.cljs
@@ -1,14 +1,34 @@
 (ns basic-component.core
     (:require [reagent.core :as reagent]))
 
-(defn home []
-  [:div
-   [foo]
-   ])
+;; From https://github.com/reagent-project/reagent-cookbook/tree/master/basics/basic-component 
 
 ;; Form-3 component
+;;
+;; The map expected by `create-class` supports two equivalent keys: `reagent-render` and `render`
+;;
 (defn foo []
-  (reagent/create-class {:reagent-render (fn [] [:div "Hello, World"])}))
+  (reagent/create-class {:render (fn [] [:div "Hello, World"])}))
+
+;; Form-1 component
+;;
+;; The most common way to create a react component: a function returning hiccup
+(defn bar []
+  [:div "Hello, World"])
+
+;; Form-2 component
+;;
+;; A final way to create a react component: a function returning a function of no arguments returning hiccup.
+(defn baz []
+  (fn []
+    [:div "Hello, World"]))
+
+(defn home []
+  [:div
+   [:h1 "Three equivalent React components"]
+   [foo]
+   [bar]
+   [baz]])
 
 (defn ^:export main []
   (reagent/render [home]
