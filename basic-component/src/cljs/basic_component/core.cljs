6745e17543c3bdb91037f88e08336857e485b04d
Larry Jones <larryj@blinds.com>
2017-04-30 18:56:16 -0500

    Change back to `reagent-render` - with a comment
    I'm unclear which information to believe, the source code, which seems
    to state that `reagent-render` and `render` are equivalent, and tutorial
    code (in particular the Day 8 wiki page on creating Reagent components)
    which states do not use `render`. (The wiki states that `render` will
    "work", but it will only accept a single argument causing particularly
    puzzling behavior.)

--- basic-component/src/cljs/basic_component/core.cljs
+++ basic-component/src/cljs/basic_component/core.cljs
@@ -7,8 +7,13 @@
 ;;
 ;; The map expected by `create-class` supports two equivalent keys: `reagent-render` and `render`
 ;;
+;; I'm a bit confused: the source code indicates that using `render` is exactly equivalent to using `reagent-render`. 
+;; (See https://github.com/reagent-project/reagent/blob/e53a5c2b1357c0560f0c4c15b28f00d09e27237b/src/reagent/core.cljs#L110.) 
+;; However, the Day-8 wiki on creating Reagent components indicate that `render` will only accept a *single* argument.
+;; (See https://github.com/Day8/re-frame/wiki/Creating-Reagent-Components.) I'm tempted to believe the comment in the
+;; source code, but I just do not know. To be safe, I'll use `reagent-render`. Sigh...
 (defn foo []
-  (reagent/create-class {:render (fn [] [:div "Hello, World"])}))
+  (reagent/create-class {:reagent-render (fn [] [:div "Hello, World"])}))
 
 ;; Form-1 component
 ;;
