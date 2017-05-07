4e0ec3209ae3a9993b7a2aa8b2424f73b8a2d17f
Larry Jones <larryj@blinds.com>
2017-05-06 22:37:55 -0500

    Routing with `accountant`

--- routing-with-accountant/project.clj
+++ routing-with-accountant/project.clj
@@ -1,7 +1,10 @@
 (defproject routing-with-accountant "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.122"]
-                 [reagent "0.5.1"]]
+                 [reagent "0.5.1"]
+                 [reagent-utils "0.2.1"]
+                 [secretary "1.2.3"]
+                 [venantius/accountant "0.2.0"]]
 
   :source-paths ["src/clj"]
 
--- routing-with-accountant/src/cljs/routing_with_accountant/core.cljs
+++ routing-with-accountant/src/cljs/routing_with_accountant/core.cljs
@@ -1,12 +1,43 @@
 (ns routing-with-accountant.core
-    (:require [reagent.core :as reagent]))
+    (:require [reagent.core :as reagent]
+              [reagent.session :as session]
+              [secretary.core :as secretary :include-macros true]
+              [accountant.core :as accountant]))
 
-(defn home []
+(enable-console-print!)
+
+;; Views
+;;
+(defn home-page []
   [:div
-   
-   ])
+   [:h1 "Home Page"]
+   [:a {:href "/about"} "There's no place like home."]])
 
+(defn about-page []
+  [:div
+   [:h1 "About Page"]
+   [:a {:href "/"} "Toto, we're not in Kansas anymore!"]])
+
+(defn current-page []
+  [:div [(session/get :current-page)]])
+
+;; Routes
+;;
+(secretary/defroute "/" []
+  (session/put! :current-page #'home-page)) 
+
+(secretary/defroute "/about" []
+  (session/put! :current-page #'about-page))
+
+;; Initialize app
+;;
 (defn ^:export main []
-  (reagent/render [home]
+  (accountant/configure-navigation! {:nav-handler (fn [path]
+                                                    (println (str "Dispatching to path " path))
+                                                    (secretary/dispatch! path))
+                                     :path-exists? (fn [path]
+                                                     (secretary/locate-route path))})
+  (accountant/dispatch-current!)
+  (reagent/render [current-page]
                   (.getElementById js/document "app")))
 
