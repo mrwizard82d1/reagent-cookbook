48f5a7e354412eaa019a5e31f8cca854a5282a98
Larry Jones <larryj@blinds.com>
2017-05-06 20:45:52 -0500

    Template project for routing with accountant.

---
+++ routing-with-accountant/src/cljs/routing_with_accountant/core.cljs
@@ -0,0 +1,12 @@
+(ns routing-with-accountant.core
+    (:require [reagent.core :as reagent]))
+
+(defn home []
+  [:div
+   
+   ])
+
+(defn ^:export main []
+  (reagent/render [home]
+                  (.getElementById js/document "app")))
+
